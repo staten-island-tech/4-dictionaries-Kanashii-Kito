541c246f989cc76ef09b85e2b0e12d0d07595f1e
Kanashii-Kito <ERicc468@nycstudents.net>
2025-10-22 13:38:46 -0400

    Update test.py

--- test.py
+++ test.py
@@ -5,3 +5,13 @@ best_buy_items = {
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
 }
 best_buy_items[0]["name"]
+
+
+
+best_buy_items = {
+    "name": "Samsung 55\" 4K UHD TV",
+    "price": 429.99,
+    "department": "Televisions",
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+}
+best_buy_items[0]["name"]
