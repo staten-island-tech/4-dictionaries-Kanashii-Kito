91efea889d217d313bd4adb754bba62bc2c8217c
Kanashii-Kito <ERicc468@nycstudents.net>
2025-10-27 10:16:20 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 sushi_menu = {
-    roll = 
-    
-
-}
-
-
-
-sushi_menu = {
     "name": "Roll.1",
     "price": 1999.99,
     "department": "Special",
     "description": "Special Roll Made From Hell."
 }
+
+
+'''----------'''
+
+Delivery_Corporation = {
+    "Name": "Route/Speed || Standard",
+    "Cost": 2499.99,
+    "Additional Charges" : "20 Dollars "
+    "Department": "Semi Truck Shipping",
+    "Description": "Packages From The Depths Hell."
+
+}
+
+
+
