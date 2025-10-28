a3c634cd977a3b046e554535e421600fa4729290
Kanashii-Kito <ERicc468@nycstudents.net>
2025-10-28 10:17:13 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,43 @@
-sushi_menu = {
-    "name": "Roll.1",
-    "price": 1999.99,
-    "department": "Special",
-    "description": "Special Roll Made From Hell."
-}
+# sushi_menu = {
+#     "name": "Roll.1",
+#     "price": 1999.99,
+#     "department": "Special",
+#     "description": "Special Roll Made From Hell."
+# }
 
 
 '''----------'''
 
-Delivery_Corporation = {
-    "Name": "Route/Speed || Standard",
-    "Cost": 2499.99,
-    "Additional Charges" : "20 Dollars "
-    "Department": "Semi Truck Shipping",
-    "Description": "Packages From The Depths Hell."
+Cart = []
 
+
+'''Delivery_Corporation'''
+Delivery_Corporation = [
+{
+    "Name": "Standard Route and Speed",
+    "Cost": 2499.99,
+    "Department": "Semi Truck Shipping", 
+    "Description": "Packages From The Depths Hell.",
 }
 
+{
+    "Name": "Premium Route and Speed",
+    "Cost": 4999.99,
+    "Department": "Semi Truck Shipping", 
+    "Description": "Faster Delivery of Packages From The Depths Hell.",
+}
+
+{
+    "Name": "Package Insurance",
+    "Cost": 99999.99,
+    "Department": "Semi Truck Shipping", 
+    "Description": "Insurance for your Packages from Hell."
+}
+]
+
+
+
+
+print()
 
 
